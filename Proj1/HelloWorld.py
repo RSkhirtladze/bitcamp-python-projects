3760daeb1b8b3bc8cc1a918f0e28425d4960eeb4
RSkhirtladze <skhitrladzerati@gmail.com>
2023-09-27 18:59:33 +0400

    Initial commit with Proj1

---
+++ Proj1/HelloWorld.py
@@ -0,0 +1,9 @@
+Name = input("What is your name? ")
+Greeting = "Hello, " + Name + ", nice to meet you"
+print(Greeting)
+
+print()
+
+print("Hello,", input("What is your name? "), ", nice to meet you")
+
+
